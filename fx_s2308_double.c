f5f72fe04b177800a31037f5749ad040f1718479
shgh2233 <shgh2233@gmail.com>
2020-08-23 16:35:08 +0900

    s2308 매크로명 통일

--- fx_s2308_double.c
+++ fx_s2308_double.c
@@ -5,19 +5,19 @@
 
 fx_s2308 fx_s2308_double_sine(fx_s2308 a)
 {
-    double f_a = FX_TO_DOUBLE(a);
+    double f_a = FX_S2308_TO_DOUBLE(a);
     f_a = TO_RADIAN(f_a);
-    return DOUBLE_TO_FX(sin(f_a));
+    return DOUBLE_TO_FX_S2308(sin(f_a));
 }
 
 fx_s2308 fx_s2308_double_sqrt(fx_s2308 a)
 {
-    double f_a = FX_TO_DOUBLE(a);
-    return DOUBLE_TO_FX(sqrt(f_a));
+    double f_a = FX_S2308_TO_DOUBLE(a);
+    return DOUBLE_TO_FX_S2308(sqrt(f_a));
 }
 
 fx_s2308 fx_s2308_double_pow(fx_s2308 a, double n)
 {
-    double f_a = FX_TO_DOUBLE(a);
-    return DOUBLE_TO_FX(pow(f_a, n));
+    double f_a = FX_S2308_TO_DOUBLE(a);
+    return DOUBLE_TO_FX_S2308(pow(f_a, n));
 }
--- headers/fx_s2308_double.h
+++ headers/fx_s2308_double.h
@@ -1,17 +1,17 @@
 #pragma once
 typedef int fx_s2308;
-#define FX_TO_DOUBLE(a) ((double)(a / (256.0)))
-#define FX_TO_FLOAT(a) ((float)(a / (256.0)))
-#define FX_TO_INT(a) ((int)(a / (256.0)))
-#define FX_TO_SHORT(a) ((short)(a / (256.0)))
-#define DOUBLE_TO_FX(a) (((int)(a * (256.0))))
-#define FX_ADD(a, b) ((a) + (b))
-#define FX_SUB(a, b) ((a) - (b))
-#define FX_MUL(a, b) DOUBLE_TO_FX(FX_TO_DOUBLE(a) * FX_TO_DOUBLE(b))
-#define FX_DIV(a, b) DOUBLE_TO_FX(FX_TO_DOUBLE(a) / FX_TO_DOUBLE(b))
+#define FX_S2308_TO_DOUBLE(a) ((double)(a / (256.0)))
+#define FX_S2308_TO_FLOAT(a) ((float)(a / (256.0)))
+#define FX_S2308_TO_INT(a) ((int)(a / (256.0)))
+#define FX_S2308_TO_SHORT(a) ((short)(a / (256.0)))
+#define DOUBLE_TO_FX_S2308(a) (((int)(a * (256.0))))
+#define FX_S2308_ADD(a, b) ((a) + (b))
+#define FX_S2308_SUB(a, b) ((a) - (b))
+#define FX_S2308_MUL(a, b) DOUBLE_TO_FX_S2308(FX_S2308_TO_DOUBLE(a) * FX_S2308_TO_DOUBLE(b))
+#define FX_S2308_DIV(a, b) DOUBLE_TO_FX_S2308(FX_S2308_TO_DOUBLE(a) / FX_S2308_TO_DOUBLE(b))
 // #define PI 3.141592
-#define FX_S2308_PI (DOUBLE_TO_FX(3.141592))
-#define FX_S2308_INV_PI (DOUBLE_TO_FX(1 / 3.141592))
+#define FX_S2308_PI (DOUBLE_TO_FX_S2308(3.141592))
+#define FX_S2308_INV_PI (DOUBLE_TO_FX_S2308(1 / 3.141592))
 
 fx_s2308 fx_s2308_double_sine(fx_s2308 a);
 fx_s2308 fx_s2308_double_sqrt(fx_s2308 a);
