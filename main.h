6194796aa52dd54b9581919cd8201b9906a7d2db
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-08-23 14:59:01 +0900

    Add main.c

--- main.h
+++ main.h
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 #include "headers/fx_s1516_double.h"
 #include "headers/fx_s1615_double.h"
 #include "headers/fx_s2308_double.h"
@@ -14,32 +12,3 @@
 #include "headers/fx_s3231_longlong.h"
 #include "headers/fx_s4716_longlong.h"
 
-#define _FX_SYSTEM
-#define _FX_CALCULATION_TYPE
-
-typedef enum fx_system {
-    S1516 = 0,
-    S1615 = 1,
-    S2308 = 2,
-    S3132 = 3,
-    S3231 = 4,
-    S4716 = 5,
-} fx_system;
-
-typedef enum fx_calculation_type {
-    FX_DOUBLE = 0,
-    FX_LONGLONG = 1,
-} fx_system;
-
-#if _FX_CALCULATION_TYPE 
-typedef fixed_t 
-
-fx_mul() {
-        
-}
-
-int main() {
-    
-
-    return 0;
-}
