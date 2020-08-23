c956253152848abbb7d58bacbe1c027e37119098
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-08-23 14:34:34 +0900

    Restructure header files.

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "headers/fx_s1516_double.h"
+#include "headers/fx_s1615_double.h"
+#include "headers/fx_s2308_double.h"
+#include "headers/fx_s3132_double.h"
+#include "headers/fx_s3231_double.h"
+#include "headers/fx_s4716_double.h"
+
+#include "headers/fx_s1516_longlong.h"
+#include "headers/fx_s1615_longlong.h"
+#include "headers/fx_s2308_longlong.h"
+#include "headers/fx_s3132_longlong.h"
+#include "headers/fx_s3231_longlong.h"
+#include "headers/fx_s4716_longlong.h"
+
+int main() {
+
+    return 0;
+}
