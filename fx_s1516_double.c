fa00a59e5dc36bd43af5518aeb55453d80a008dd
shgh2233 <shgh2233@gmail.com>
2020-08-23 16:30:02 +0900

    s1516 함수 반환형 통일

--- fx_s1516_double.c
+++ fx_s1516_double.c
@@ -1,34 +1,34 @@
 // fx_s1516_double.c file
 #include "fx_s1516_double.h"
 
-fx_s1516 double_to_fx(double a)
+fx_s1516 double_to_fx_s1516(double a)
 {
     return (int)(((a)*P2_16));
 }
 
-double fx_to_double(fx_s1516 a)
+double fx_s1516_to_double(fx_s1516 a)
 {
     return ((double)(a) / P2_16);
 }
 
-double fx_s1516_double_add(double a, double b)
+fx_s1516 fx_s1516_double_add(double a, double b)
 {
-    return double_to_fx((a) + (b));
+    return double_to_fx_s1516((a) + (b));
 }
 
-double fx_s1516_double_sub(double a, double b)
+fx_s1516 fx_s1516_double_sub(double a, double b)
 {
-    return double_to_fx((a) - (b));
+    return double_to_fx_s1516((a) - (b));
 }
 
-double fx_s1516_double_mul(double a, double b)
+fx_s1516 fx_s1516_double_mul(double a, double b)
 {
-    return double_to_fx((a) * (b)*P2_16);
+    return double_to_fx_s1516((a) * (b)*P2_16);
 }
 
-double fx_s1516_double_div(double a, double b)
+fx_s1516 fx_s1516_double_div(double a, double b)
 {
-    return double_to_fx((a) / (b) / P2_16);
+    return double_to_fx_s1516((a) / (b) / P2_16);
 }
 
 double degree_to_radian(double a)
@@ -36,27 +36,29 @@ double degree_to_radian(double a)
     return ((a) * FX_S1516_PI / 180);
 }
 
-double fx_s1516_double_sin(double a)
+fx_s1516 fx_s1516_double_sin(double a)
 {
-    return double_to_fx(sin(a * P2_16) / (P2_16));
+    return double_to_fx_s1516(sin(a * P2_16) / (P2_16));
 }
 
-double fx_s1516_double_sqrt(double a)
+fx_s1516 fx_s1516_double_sqrt(double a)
 {
-    return double_to_fx(sqrt(a) / sqrt(P2_16));
+    return double_to_fx_s1516(sqrt(a) / sqrt(P2_16));
 }
 
-double fx_s1516_double_pow(double a, double b)
+fx_s1516 fx_s1516_double_pow(double a, double b)
 {
-    return double_to_fx(pow(a, (double)b) * pow(P2_16, (double)b - 1));
+    return double_to_fx_s1516(pow(a, (double)b) * pow(P2_16, (double)b - 1));
 }
 
-double fx_s1516_double_log(double a, double b)
-{
-    return double_to_fx(baselog(a * P2_16, b) / (P2_16));
-}
-
-double baselog(double a, double base)
+static double baselog(double a, double base)
 {
     return log(a) / log(base);
 }
+
+fx_s1516 fx_s1516_double_log(double a, double b)
+{
+    return double_to_fx_s1516(baselog(a * P2_16, b) / (P2_16));
+}
+
+
--- headers/fx_s1516_double.h
+++ headers/fx_s1516_double.h
@@ -11,15 +11,15 @@ typedef int fx_s1516;
 #define FX_S1516_PI M_PI
 #define FX_S1516_INVERSE_PI 1 / M_PI
 
-extern fx_s1516 double_to_fx(double a);
-extern double fx_to_double(fx_s1516 a);
-extern double baselog(double a, double base);
-extern double fx_s1516_double_add(double a, double b);
-extern double fx_s1516_double_sub(double a, double b);
-extern double fx_s1516_double_mul(double a, double b);
-extern double fx_s1516_double_div(double a, double b);
-extern double fx_s1516_double_sqrt(double a);
-extern double fx_s1516_double_pow(double a, double b);
-extern double fx_s1516_double_log(double a, double b);
+extern fx_s1516 double_to_fx_s1516(double a);
+extern double fx_s1516_to_double(fx_s1516 a);
+// extern double baselog(double a, double base);
+extern fx_s1516 fx_s1516_double_add(double a, double b);
+extern fx_s1516 fx_s1516_double_sub(double a, double b);
+extern fx_s1516 fx_s1516_double_mul(double a, double b);
+extern fx_s1516 fx_s1516_double_div(double a, double b);
+extern fx_s1516 fx_s1516_double_sqrt(double a);
+extern fx_s1516 fx_s1516_double_pow(double a, double b);
+extern fx_s1516 fx_s1516_double_log(double a, double b);
 extern double degree_to_radian(double a);
-extern double fx_s1516_double_sin(double a);
+extern fx_s1516 fx_s1516_double_sin(double a);
